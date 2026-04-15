1c46dd53c19869758672294cab7c1cae62eb4849
roundcap <knd522@york.ac.uk>
2026-04-15 11:09:41 +0100

    forgot to do any commits. it is basically finished butthis is my first commit oops!

--- R/todo.R
+++ R/todo.R
@@ -3,18 +3,70 @@ suppressPackageStartupMessages({
   library(argparse)
 })
 
-TASK_FILE <- ".tasks.txt" # nolint
+TASK_FILE <- "test_list.txt" # nolint
+
+clean_todo_list <- function() {
+  # 1. Read the file
+  todo_list_file <- readLines(TASK_FILE, warn = FALSE)
+  
+  # 2. Clean the file (remove spaces and empty lines)
+  todo_list_file <- trimws(todo_list_file)
+  todo_list_file <- todo_list_file[todo_list_file != ""]
+  
+  # 3. Return the cleaned data
+  return(todo_list_file)
+}
+
+#counts how many tasks are in the file
+count_todo_list <- function() {
+  todo_list_file <- clean_todo_list()
+  task_count <- length(todo_list_file)
+  
+  return(task_count)
+}
 
 add_task <- function(task) {
-
+  cat(task, file = TASK_FILE, append = TRUE, sep = "\n") #adds 'task' to the file
+  cat("You have added a new task, you have", paste(count_todo_list()), "tasks to do!", "\n")
 }
 
 list_tasks <- function() {
-
+  todo_list_file <- clean_todo_list()
+  
+  if (length(todo_list_file) == 0) { #checks if there are tasks in the file
+    output <- "Your todo list is empty, it's time to relax!"
+    cat(output, "\n")
+  } else {
+  line_numbers <- 1:length(todo_list_file) #make sequence of numbers the same length as the list
+  numbered_list <- paste(line_numbers, todo_list_file, sep = ". ") #combine numbers and list
+  
+  output <- paste(numbered_list, collapse = "\n") #for the test
+  
+  #for user
+  cat("You have", count_todo_list(), "tasks. Here is your todo list:\n")
+  cat(numbered_list, sep = "\n")
+  }
+  return(output) #for test
 }
 
 remove_task <- function(index) {
-
+  todo_list_file <- clean_todo_list()
+  
+  # Check if the index is valid
+  if (index > length(todo_list_file) | index <= 0) {
+    print("Error: Task number", index, "does not exist.\n")
+  } else {
+    # Remove the task using negative indexing
+    updated_list <- todo_list_file[-index]
+    
+    # Write the updated list back to the file
+    # We use writeLines here because it replaces the whole file 
+    # with our new version that is missing the deleted task.
+    writeLines(updated_list, TASK_FILE)
+    
+    cat("Task", index, "removed successfully!\n")
+    cat("You have", count_todo_list(), "tasks left to go!")
+  }
 }
 
 main <- function(args) {
@@ -22,12 +74,11 @@ main <- function(args) {
   if (!is.null(args$add)) {
     add_task(args$add)
   } else if (args$list) {
-    tasks <- list_tasks()
-    print(tasks)
+    list_tasks()
   } else if (!is.null(args$remove)) {
-    remove_task(args$remove)
+    remove_task(as.numeric(args$remove))
   } else {
-    print("Use --help to get help on using this program")
+    cat("Use --help to get help on using this program")
   }
 }
 
@@ -47,3 +98,4 @@ if (sys.nframe() == 0) {
   args <- parser$parse_args()
   main(args)
 }
+
